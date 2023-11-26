0505bf8109f7d8c83dff1f34681b7416647a3d53
Norton0213 <92254437+Norton0213@users.noreply.github.com>
2023-11-26 17:09:48 +0800

    Update go.py

--- go.py
+++ go.py
@@ -2,5 +2,9 @@ from MMSA import MMSA_run
 
 
 
-# run MTFN on SIMS with custom config file
-MMSA_run('misa', 'sims', config_file='./config/config_regression.json')
+
+MMSA_run('mavmfa_conformer', 'sims',
+         config_file='./config/config_regression.json',
+         #custom_feature='D:\PycharmProjects\MMSA-master\src\Featureoutput\\sims_try_3.pkl',
+         #feature_V='../Featureoutput/mosi_try_1.pkl'
+         )
